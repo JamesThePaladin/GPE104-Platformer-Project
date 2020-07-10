10b1488b6b201f628400a3d12aac7c8507067cd5
JamesThePaladin <Jamesthepaladin@gmail.com>
2020-07-09 23:27:22 -0700

    Big Update
    fixed animations, changed level assets, added camera follow. adjusted player settings to feel better

---
+++ Packages/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Controller
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (pawn == null)
+        {
+            pawn = GameManager.instance.player.GetComponent<Pawn>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //movement inputs
+        if (Input.GetKey(KeyCode.A))
+        {
+            pawn.Move(-pawn.transform.right);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            pawn.Move(pawn.transform.right);
+        }
+
+        //jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            pawn.Jump();
+        }
+    }
+}
+
