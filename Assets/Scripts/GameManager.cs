7b62f25a6c6a4e8b45bcc130a69e46f7302b9d71
JamesThePaladin <Jamesthepaladin@gmail.com>
2020-07-09 21:26:02 -0700

    Animation Update
    Animation has been added. Movement was added in a previous commit but the github app in Unity now has a different history so this is the first commit online.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance; //variable that holds this instance of the GameManager
+
+    public GameObject player; //variable for player
+    private Transform playerTf; //variable for player's transform
+
+    public int score; //public player score for testing
+    public int lives; //lives for player
+    //public Text scoreText; //reference to score text
+    //public Text livesText; //reference to lives text
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null) // if instance is empty
+        {
+            instance = this; // store THIS instance of the class in the instance variable
+            DontDestroyOnLoad(this.gameObject); //keep this instance of game manager when loading new scenes
+        }
+        else
+        {
+            Destroy(this.gameObject); // delete the new game manager attempting to store itself, there can only be one.
+            Debug.Log("Warning: A second game manager was detected and destrtoyed"); // display message in the console to inform of its demise
+        }
+
+        if (player == null) //if player slot is empty
+        {
+            player = GameObject.FindWithTag("Player"); //fill it with player
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player == null) //if player slot is empty
+        {
+            player = GameObject.FindWithTag("Player"); //fill it with player
+        }
+    }
+}
---
+++ Assets/Scripts/Pawn.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class Pawn : MonoBehaviour
+{
+    //for pawn's animator
+    public Animator anim;
+    //for pawn's Rigidbody2D
+    public Rigidbody2D rb;
+    //game object's sprite renderer
+    public SpriteRenderer sr;
+
+    //for paw speed
+    public float speed;
+    //for pawn jump height
+    public float jumpForce;
+    //float for grounding distance
+    public float groundingDistance;
+
+    [Header("Animation Thresholds")]
+    //float for jump animation threshold
+    public float yThreshold;
+    //float for walk animation threshold
+    public float xThreshold;
+    
+
+
+    void Start()
+    {
+        //get components of game object
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        UpdateAnimations();
+    }
+
+    //**************************
+    //TODO fix walk animation
+    //**************************
+
+    //method for animations
+    public void UpdateAnimations() 
+    {
+        if (rb.velocity.x > 0 && rb.velocity.y == 0)
+        {
+            sr.flipX = false;
+            anim.Play("Player1Walk2");
+        }
+        else if (rb.velocity.x < 0 && rb.velocity.y == 0)
+        {
+            sr.flipX = true;
+            anim.Play("Player1Walk2");
+        }
+        else if (rb.velocity.y > 0 && rb.velocity.x < 0)
+        {
+            sr.flipX = true;
+            anim.Play("Player1Jump");
+        }
+        else if (rb.velocity.y > 0 && rb.velocity.x > 0)
+        {
+            sr.flipX = false;
+            anim.Play("Player1Jump");
+        }
+        else if (rb.velocity.y < 0 && rb.velocity.x < 0)
+        {
+            sr.flipX = true;
+            anim.Play("Player1Jump");
+        }
+        else if (rb.velocity.y < 0 && rb.velocity.x > 0)
+        {
+            sr.flipX = false;
+            anim.Play("Player1Jump");
+        }
+        else
+        {
+            anim.Play("Player1Idle");
+        }
+    }
+
+    public void Move(Vector3 direction)
+    {
+        //move the rigidbody by x input multiplied by speed. pass y axis 0
+        rb.velocity = new Vector3(direction.x * speed, rb.velocity.y, 0);
+    }
+
+    public void Jump() 
+    {
+        if (IsGrounded()) 
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, 0);
+        }
+    }
+
+    public bool IsGrounded() 
+    {
+        //check if we are grounded
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, groundingDistance);
+        if (hitInfo.collider.gameObject.CompareTag("Ground"))
+        {
+            return true;
+        }
+
+        else 
+        {
+            return false;
+        }
+    }
+}
